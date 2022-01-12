bdcdb80af3e3bd0d7a3c6e4c46173562d4232432
5326987143287 <90911935+5326987143287@users.noreply.github.com>
2022-01-12 13:36:17 -0500

    Create lvl5.js

---
+++ Unit5/lesson3/lvl5.js
@@ -0,0 +1,35 @@
+// Create and assign variables
+var index = 0;
+var classes = ["Biology", "Algebra", "Social Studies","Gym"];
+updateScreen();
+
+onEvent("leftButton", "click", function(){
+	// use the Scroll Pattern to decrease the index
+	
+	// call updateScreen()
+
+	playSound("sound://category_accent/puzzle_game_accent_small_d_01.mp3");
+});
+
+onEvent("rightButton", "click", function(){
+  
+  if(index < (classes.length - 1)){
+    index = index + 1;
+  }
+  updateScreen();
+	playSound("sound://category_accent/puzzle_game_accent_small_d_02.mp3");
+});
+
+onEvent("leftButton", "click", function(){
+  
+  if(index < (classes.length + 1)){
+    index = index - 1;
+  }
+  updateScreen();
+	playSound("sound://category_accent/puzzle_game_accent_small_d_02.mp3");
+});
+
+function updateScreen(){
+	setText("classLabel", classes[index]);
+	setText("periodLabel", "Period: " + (index + 1));
+}

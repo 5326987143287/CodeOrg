3d80b5cf168a4ff15045bce1b605840efb268d96
5326987143287 <90911935+5326987143287@users.noreply.github.com>
2022-01-12 13:35:34 -0500

    Create lvl3.js

---
+++ Unit5/lesson3/lvl3.js
@@ -0,0 +1,13 @@
+var animals = ["cat", "dog", "bird", "raccoon", "bear", "rabbit", "coyote", "deer", "buffalo", "lizard", "turtle"];
+
+console.log("The first animal in the list is a: ");
+console.log(animals[0]);
+
+console.log("The third animal in the list is a: ");
+console.log(animals[2]);
+
+console.log("The fifth animal in the list is a: ");
+console.log(animals[4]);
+
+console.log("The last animal in the list is a: ");
+console.log(animals[10]);

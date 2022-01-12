e9209f1329b9969414787459dfebac6b1add6b00
5326987143287 <90911935+5326987143287@users.noreply.github.com>
2022-01-12 13:38:01 -0500

    Create lvl8.js

---
+++ Unit5/lesson3/lvl8.js
@@ -0,0 +1,17 @@
+var food = ["carrots", "pizza", "lettuce", "strawberries"];
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+appendItem(food, "peanuts");
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+appendItem(food,"another pizza");
+
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+appendItem(food,"another pizza");
+
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);

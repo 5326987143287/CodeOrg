4ea8dae31def85de0aad13a200f2da6b3886b94a
5326987143287 <90911935+5326987143287@users.noreply.github.com>
2022-01-12 13:37:14 -0500

    Create lvl7.js

---
+++ Unit5/lesson3/lvl7.js
@@ -0,0 +1,8 @@
+var date = "05/21/1994";
+var month = date.substring(1,3);
+var day = date.substring(2,5);
+var year = date.substring(date.length-4,date.length-1);
+
+console.log("Month: " + month);
+console.log("Day: " + day);
+console.log("Year: " + year);

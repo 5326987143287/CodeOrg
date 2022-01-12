23c91b57c10da2336180e0bb5af9f02c32571c08
5326987143287 <90911935+5326987143287@users.noreply.github.com>
2022-01-12 13:35:12 -0500

    Create lvl2.js

---
+++ Unit5/lesson3/lvl2.js
@@ -0,0 +1,8 @@
+console.log("Trees:");
+var trees = ["oak", "maple", "dogwood"];
+var songs = ["not a song1", "still not a song2", "just some random stuff3"];
+console.log(trees);
+console.log("");
+
+console.log("My Favorite Songs:");
+console.log(songs);
